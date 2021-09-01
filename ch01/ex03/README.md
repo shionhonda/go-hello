01fa6fcddde03287fef4f03bc7dcb51e7c413c34
Shion <26x.orc.ed5.1hs@gmail.com>
2021-09-01 10:38:02 +0900

    ex03

---
+++ ch01/ex03/README.md
@@ -0,0 +1,5 @@
+```
+$ go run benchmark_echo.go
+My Join: 0.000903 s
+Native Join: 0.000016 s
+```
--- ch01/ex03/benchmark_echo.go
+++ ch01/ex03/benchmark_echo.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"strings"
 	"time"
 )
 
-func myJoin(list []string) string {
-	s, sep := "", ""
-	for _, arg := range list {
-		s += sep + arg
-		sep = " "
+func createStringArray(n int) []string {
+	array := make([]string, 0)
+	for i := 0; i < n; i++ {
+		array = append(array, "a")
+	}
+	return array
+}
+
+func myJoin(array []string, sep string) string {
+	s := ""
+	for i, arg := range array {
+		if i == 0 {
+			s += arg
+		} else {
+			s += sep + arg
+		}
 	}
 	return s
 }
 
-func main() {
+func timer(f func([]string, string) string, array []string) float64 {
 	start := time.Now()
-	s := myJoin(os.Args)
+	_ = f(array, " ")
 	secs := time.Since(start).Seconds()
+	return secs
+}
+
+func main() {
+	array := createStringArray(1000)
+
+	secs := timer(myJoin, array)
+	fmt.Printf("My Join: %.6f s\n", secs)
+
+	secs = timer(strings.Join, array)
+	fmt.Printf("Native Join: %.6f s\n", secs)
 }
