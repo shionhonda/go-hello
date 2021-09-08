86734395f050f003e78946d390b7a3af618c2a7c
Shion <26x.orc.ed5.1hs@gmail.com>
2021-09-08 10:33:00 +0900

    ex09

---
+++ ch01/ex09/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func formatUrl(url string) string {
+	prefix := "http://"
+	if strings.HasPrefix(url, prefix) {
+		return url
+	} else {
+		return prefix + url
+	}
+}
+
+func main() {
+	for _, url := range os.Args[1:] {
+		url = formatUrl(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Status Code: %s\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
