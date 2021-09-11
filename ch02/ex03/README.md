2cd00594cc38b7bfbfc43992829934664eb65de1
Shion <26x.orc.ed5.1hs@gmail.com>
2021-09-11 19:37:35 +0900

    ex03

---
+++ ch02/ex03/README.md
@@ -0,0 +1,7 @@
+Popcount with loop is slower.
+
+```console
+$ go run ch02/ex03/main.go 123456
+Base: 0.004697s
+Loop: 0.013799s
+```
--- ch02/ex03/main.go
+++ ch02/ex03/main.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	"go-hello/ch02/ex03/popcount"
 )
 
-func main() {
-	for _, arg := range os.Args[1:] {
-		x, err := strconv.Atoi(arg)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Cannot convert to integer: %s", arg)
-		}
-		c := popcount.PopCountLoop(uint64(x))
-		fmt.Printf("%d\n", c)
+func bench(f func(uint64) int, x uint64, n int) float64 {
+	start := time.Now()
+	for i := 0; i < n; i++ {
+		f(x)
 	}
+	secs := time.Since(start).Seconds()
+	return secs
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "You must specify at least one argument")
+		os.Exit(1)
+	}
+	arg := os.Args[1]
+	x, err := strconv.Atoi(arg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot convert to integer: %s", arg)
+	}
+	n_trials := 1000000
+	t := bench(popcount.PopCount, uint64(x), n_trials)
+	fmt.Printf("Base: %.6fs\n", t)
+
+	t = bench(popcount.PopCountLoop, uint64(x), n_trials)
+	fmt.Printf("Loop: %.6fs\n", t)
 }
