3c91451e01248e3fa511ac698f68df952b39eb9d
Shion <26x.orc.ed5.1hs@gmail.com>
2021-09-11 22:45:25 +0900

    ex05

---
+++ ch02/ex04/README.md
@@ -0,0 +1,8 @@
+Popcount with loop is even slower.
+
+```console
+# Time for 1,000,000 loops
+$ go run ch02/ex04/main.go 123456
+Base: 0.004697s
+Loop: 0.041804s
+```
---
+++ ch02/ex04/popcount/counter.go
@@ -0,0 +1,30 @@
+package popcount
+
+// pc[i] is a population count of i
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountLoop64(x uint64) int {
+	y := 0
+	for i := 0; i < 64; i++ {
+		y += int(byte(x & 1))
+		x = x >> 1
+	}
+	return y
+}
