3dce6d6dafeebba23953f2ffd072232ea0cca3e9
Shion <26x.orc.ed5.1hs@gmail.com>
2021-10-11 11:23:40 +0900

    red

--- ch03/ex01/main.go
+++ ch03/ex01/main.go
@@ -20,29 +20,29 @@ func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay, err := corner(i+1, j)
-			if err != nil {
-				continue
-			}
-			bx, by, err := corner(i, j)
-			if err != nil {
-				continue
-			}
-			cx, cy, err := corner(i, j+1)
-			if err != nil {
-				continue
-			}
-			dx, dy, err := corner(i+1, j+1)
-			if err != nil {
-				continue
-			}
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
-		}
-	}
-	fmt.Println("</svg>")
+	// for i := 0; i < cells; i++ {
+	// 	for j := 0; j < cells; j++ {
+	// 		ax, ay, err := corner(i+1, j)
+	// 		if err != nil {
+	// 			continue
+	// 		}
+	// 		bx, by, err := corner(i, j)
+	// 		if err != nil {
+	// 			continue
+	// 		}
+	// 		cx, cy, err := corner(i, j+1)
+	// 		if err != nil {
+	// 			continue
+	// 		}
+	// 		dx, dy, err := corner(i+1, j+1)
+	// 		if err != nil {
+	// 			continue
+	// 		}
+	// 		fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+	// 			ax, ay, bx, by, cx, cy, dx, dy)
+	// 	}
+	// }
+	// fmt.Println("</svg>")
 }
 
 func corner(i, j int, f func(float64, float64) float64) (float64, float64, error) {
--- ch03/ex01/main_test.go
+++ ch03/ex01/main_test.go
@@ -3,33 +3,33 @@ package main
 import (
 	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+type testCase struct {
+	title string
+	f     func(float64, float64) float64
+	isErr bool
+}
+
 func TestCorner(t *testing.T) {
-	asserts := assert.New(t)
-	for _, td := range []struct {
-		title  string
-		input  []int
-		output int
-	}{
-		{
-			title:  "2×3の答えが6になる",
-			input:  []int{2, 3},
-			output: 6,
-		},
-		{
-			title:  "3×5の答えが15になる",
-			input:  []int{3, 5},
-			output: 15,
-		},
-	} {
-		t.Run("Square:"+td.title, func(t *testing.T) {
-			_, _, err := corner(td.input[0], td.input[1])
-			asserts.Equal(td.output, result)
+	testCases := []testCase{
+		{"No error", pi, false},
+		{"Positive infinity", posInf, true},
+		{"Negative infinity", negInf, true},
+		{"NaN", nan, true},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			_, _, err := corner(0, 0, tc.f)
+			if tc.isErr {
+				assert.NotEqual(t, nil, err)
+			} else {
+				assert.Equal(t, nil, err)
+			}
 		})
 	}
-	x, y, err := corner(0, 0, pi)
-
 }
 
 func pi(x, y float64) float64 {
