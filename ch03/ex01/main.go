ae88b080205ebce68c8a0b3baf3bdfd43d603492
Shion <26x.orc.ed5.1hs@gmail.com>
2021-10-11 11:29:30 +0900

    ex01

--- ch03/ex01/main.go
+++ ch03/ex01/main.go
@@ -20,41 +20,41 @@ func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
-	// for i := 0; i < cells; i++ {
-	// 	for j := 0; j < cells; j++ {
-	// 		ax, ay, err := corner(i+1, j)
-	// 		if err != nil {
-	// 			continue
-	// 		}
-	// 		bx, by, err := corner(i, j)
-	// 		if err != nil {
-	// 			continue
-	// 		}
-	// 		cx, cy, err := corner(i, j+1)
-	// 		if err != nil {
-	// 			continue
-	// 		}
-	// 		dx, dy, err := corner(i+1, j+1)
-	// 		if err != nil {
-	// 			continue
-	// 		}
-	// 		fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-	// 			ax, ay, bx, by, cx, cy, dx, dy)
-	// 	}
-	// }
-	// fmt.Println("</svg>")
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, err := corner(i+1, j, f)
+			if err != nil {
+				continue
+			}
+			bx, by, err := corner(i, j, f)
+			if err != nil {
+				continue
+			}
+			cx, cy, err := corner(i, j+1, f)
+			if err != nil {
+				continue
+			}
+			dx, dy, err := corner(i+1, j+1, f)
+			if err != nil {
+				continue
+			}
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
 }
 
 func corner(i, j int, f func(float64, float64) float64) (float64, float64, error) {
-	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
-	// Compute surface height z.
 	z := f(x, y)
-	// if math.IsInf(z) || math.I
+	// Return error if z in Inf or NaN
+	if math.IsInf(z, 0) || math.IsNaN(z) {
+		return 0., 0., fmt.Errorf("Invalid output: %v", z)
+	}
 
-	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 	return sx, sy, nil
