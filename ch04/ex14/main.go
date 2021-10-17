607df23d24f19840d95b3305e0479d80c9642aa1
Shion <26x.orc.ed5.1hs@gmail.com>
2021-10-17 21:55:36 +0900

    ex14

--- ch04/ex14/main.go
+++ ch04/ex14/main.go
@@ -1,46 +1,43 @@
-// Reports GitHub issues in the last month, year, and all-time.
-
 package main
 
 import (
-	"fmt"
 	"go-hello/ch04/ex10/github"
+	"html/template"
 	"log"
-	"time"
+	"os"
 )
 
+//!+template
+
+var issueList = template.Must(template.New("issuelist").Parse(`
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='text-align: left'>
+  <th>#</th>
+  <th>State</th>
+  <th>User</th>
+  <th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+  <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+  <td>{{.State}}</td>
+  <td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+  <td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
+
+//!-template
+
+//!+
 func main() {
-	res, err := github.SearchIssues([]string{"repo:golang/go is:open json decoder"})
+	result, err := github.SearchIssues([]string{"repo:golang/go", "is:open", "json", "decoder"})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Found %v issues.\n", res.TotalCount)
-	now := time.Now()
-
-	var iMonth, iYear, iAll []*github.Issue
-	for _, item := range res.Items {
-		days := now.Sub(item.CreatedAt).Hours() / 24
-		if days <= 30. {
-			iMonth = append(iMonth, item)
-		} else if days <= 365. {
-			iYear = append(iYear, item)
-		} else {
-			iAll = append(iAll, item)
-		}
-	}
-
-	fmt.Println("\nIssues created in the last month:")
-	for _, item := range iMonth {
-		fmt.Printf("#%-5d %9.9s %55s\n", item.Number, item.User.Login, item.Title)
-	}
-
-	fmt.Println("\nIssues created in the last year:")
-	for _, item := range iYear {
-		fmt.Printf("#%-5d %9.9s %55s\n", item.Number, item.User.Login, item.Title)
-	}
-
-	fmt.Println("\nIssues Older:")
-	for _, item := range iAll {
-		fmt.Printf("#%-5d %9.9s %55s\n", item.Number, item.User.Login, item.Title)
+	if err := issueList.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
 	}
 }
