e1cdda994e63840b01450d0acbcefb9099fddf2f
Shion Honda <shionhonda@users.noreply.github.com>
2022-02-13 21:42:59 +0900

    ch12-2 (#17)
    * ex01
    
    * ex02
    
    * ex03
    
    * ex04
    
    * copy
    
    * ex01
    
    * ex02
    
    * ex03
    
    * working
    
    * ex04
    
    * work
    
    * ex05
    
    * ex06
    
    * ex07
    
    * ex08
    
    * ex09 WIP
    
    * ex10
    
    * ex11
    
    * wip ex12
    
    * ex12
    
    * ex13

---
+++ ch12/ex09/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"go-hello/ch12/ex09/sexpr"
+	"log"
+	"reflect"
+)
+
+type Movie struct {
+	Title, Subtitle string
+	Year            int
+	Actor           map[string]string
+	Oscars          []string
+	Sequel          *string
+}
+
+func main() {
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	// encode
+	data, err := sexpr.Marshal(strangelove)
+	if err != nil {
+		panic(err)
+	}
+
+	// decode
+	var movie Movie
+	err = sexpr.Unmarshal(data, &movie)
+	if err != nil {
+		log.Fatalf("Unmarshal failed: %v", err)
+	}
+	if !reflect.DeepEqual(movie, strangelove) {
+		log.Fatal("not equal")
+	} else {
+		fmt.Println("successfully reconstructed")
+	}
+}
